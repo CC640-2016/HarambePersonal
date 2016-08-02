85524256341e318ea496020326513db46b945980
clg_pingu <jmanuel.aguilerai@gmail.com>
2016-08-02 19:21:03 +0000

    a ver que onda el travis

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 
 def create_task(request):
+    return render(request, 'create_task_view.html', {})
+
+def save_task(request):
+    description = request.POST.get('description')
+    if description != '':
+        task = Task(description=description)
+        task.save()
+    else:
+        # tirar error
+        pass
+        
     return render(request, 'create_task_view.html', {})
