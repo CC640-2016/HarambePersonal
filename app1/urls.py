79f1dcb6981bc949bff96c65c1dca3684770812c
clg_pingu <jmanuel.aguilerai@gmail.com>
2016-08-10 00:30:40 +0000

    list filtering

--- app1/urls.py
+++ app1/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^task_edition/(?P<task_id>[0-9]+)$', views.task_edition, name='edit_task_view'),
     url(r'^edit_task/(?P<task_id>[0-9]+)$', views.edit_task, name='edit_task'),
     url(r'^delete_task/(?P<task_id>[0-9]+)$', views.delete_task, name='delete_task'),
-    url(r'^finish_task/(?P<task_id>[0-9]+)$', views.finish_task, name='finish_task')
+    url(r'^finish_task/(?P<task_id>[0-9]+)$', views.finish_task, name='finish_task'),
+    url(r'^task_list/finished$', views.list_tasks_finished, name='task_list_finished'),
 ]
